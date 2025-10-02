19a4e8bccf2acc5545002c9285cc06c162cf654d
Alessandro Milán <alessandromilanortega@gmail.com>
2025-10-02 17:40:39 -0600

    Add password reset workflow

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,16 @@ from . import views
 urlpatterns = [
     path("login/",  auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"),
+        name="password_reset_done",
+    ),
 
     path("settings/", views.account_settings, name="account_settings"),
 
